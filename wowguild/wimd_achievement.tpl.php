ed021722f3aa955a32179ad2d24e74451edeab09
Joh739 <Joh739@c24b908e-d626-11de-a63d-2763023b5f98>
2009-12-21 22:25:26 +0000

    Changed module's system name from wowguild to wimd.

---
+++ wowguild/wimd_achievement.tpl.php
@@ -0,0 +1,26 @@
+<?php
+// $Id$
+/**
+ * Template to display a character page.
+ *
+ * Fields available:
+ * $name: cleaned plain text string
+ * $level: cleaned plain text string
+ * $genderId: cleaned plain text string
+ * $classId: cleaned plain text string
+ * $raceId: cleaned plain text string
+ * $gender: cleaned plain text string
+ * $class: cleaned plain text string
+ * $race: cleaned plain text string
+ * $achPoints: cleaned plain text string
+ */
+?>
+
+<div id="wimd_achievement_wrapper">
+<div id="wimd_achievement_style">
+<div id="wimd_achievement_points"><?php print $points; ?></div>
+<div id="wimd_achievement_date"><?php print $date; ?></div>
+<div id="wimd_achievement_title"><?php print $title; ?></div>
+<div id="wimd_achievement_body"><?php print $body; ?></div>
+</div></div>
+
